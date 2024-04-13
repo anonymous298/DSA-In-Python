c8e7e14974b85a551ac81d1e678f5a11fef92a6f
anonymous298 <tackletalha@gmail.com>
2024-04-13 11:05:22 +0500

    add new methods

--- Doubly_linkedList.py
+++ Doubly_linkedList.py
@@ -55,3 +55,47 @@ class DoublyLL:
             count += 1
             pointer = pointer.next
     
+    def traverse_forward(self):
+        if self.head is None:
+            print("Doubly LL is empty")
+
+        pointer = self.head
+        llstr = ''
+        while pointer:
+            arrow = ''
+            if pointer.next:
+                arrow = '-->'
+            llstr += str(pointer.data) + arrow
+            pointer = pointer.next
+
+        print(llstr)
+
+    def traverse_backward(self):
+        if self.head is None:
+            print("Doubly LL is empty")
+
+        pointer = self.head
+        llstr = ''
+        
+        while pointer.next:
+            pointer = pointer.next
+
+        while pointer:
+            arrow = ''
+            if pointer.prev:
+                arrow = '<--'
+            llstr += str(pointer.data) + arrow
+            pointer = pointer.prev
+
+        print(llstr)
+
+if __name__ == "__main__":
+
+    dll = DoublyLL()
+    dll.insert_at_begining(4)
+    dll.insert_at_end(5)
+    dll.insert_at_end(6)
+    dll.insert_at_end(7)
+    dll.insert_at(10,1)
+    dll.traverse_forward()
+    dll.traverse_backward()
