8903ff5aa4134d04d4a588acccf63ca16152f863
anonymous298 <tackletalha@gmail.com>
2024-04-13 10:50:06 +0500

    add Insert at index method

--- Doubly_linkedList.py
+++ Doubly_linkedList.py
@@ -8,7 +8,7 @@ class DoublyLL:
     def __init__(self):
         self.head = None
 
-    def add_at_begining(self, data):
+    def insert_at_begining(self, data):
         if self.head is None:
             node = Node(data, self.head)
             self.head = node
@@ -18,7 +18,7 @@ class DoublyLL:
             self.head.prev = node
             self.head = node
 
-    def add_at_end(self, data):
+    def insert_at_end(self, data):
         if self.head is None:
             node = Node(data, self.head)
             self.head = node
@@ -28,4 +28,30 @@ class DoublyLL:
             pointer = pointer.next
 
         pointer.next = Node(data, None, pointer)
+
+    def get_length(self):
+        length = 0
+        pointer = self.head
+        while pointer:
+            length += 1
+            pointer = pointer.next
+
+        return length
+
+    def insert_at(self, data, index):
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid Index")
+        
+        count = 0
+        pointer = self.head
+
+        while pointer:
+            if count == index - 1:
+                node = Node(data, pointer.next,pointer)
+                pointer.next.prev = node
+                pointer.next = node
+
+
+            count += 1
+            pointer = pointer.next
     
